ab195f04be6bed3a1db19f0f76ee21bd72a85008
jinwon <kjw55523@gmail.com>
2022-05-09 17:00:20 +0900

    Feat: Api,@ResponseBody(#005)
      Api의 방식의 객체반환과정
    
      -helloController.java

--- src/main/java/hello/hellospring/controller/helloController.java
+++ src/main/java/hello/hellospring/controller/helloController.java
@@ -1,10 +1,12 @@
 package hello.hellospring.controller;
 
 
+import hello.hellospring.HelloSpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class helloController {
@@ -20,4 +22,32 @@ public class helloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloAPi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        System.out.print("d" + hello);
+        return hello; //json 형식으로 return {키: value}
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() { //java bean 규약 property 접근방식
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
