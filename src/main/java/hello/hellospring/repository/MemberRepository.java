84255f49c23ff8bdd0a6d3aa688f36b80ab5bc9d
jinwon <kjw55523@gmail.com>
2022-07-21 14:48:51 +0900

    Feat: 회원 도메인과 리포지토리 만들기(#006)
      -Member.java
      -MemberRepository.java
      -MemoryMemberRepository.java

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domian.Member;
+
+import java.util.Optional;
+import java.util.List;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
